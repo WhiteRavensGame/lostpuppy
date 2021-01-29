36a5812d4f3fffd8d6199c8d9afdd223c4b99165
cuecharles <cuecharles@gmail.com>
2021-01-29 12:14:05 -0800

    Updated the Rotate Script

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -7,12 +7,13 @@ public class Rotate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("HELLO WORLD");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(1,1,1)); 
+        //Hello! Change me, ask Charles
+        transform.Rotate(new Vector3(2,2,2)); 
     }
 }
