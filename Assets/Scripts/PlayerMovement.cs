ae217044c81f88e30e8a597c290ea6c9efaf6a47
NIFLHEIM-FROST-\Deant <dean.toledano@gmail.com>
2021-01-30 14:47:33 -0800

    no message

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private int keysPressed = 0;
+    public float movementSpeed;
+
+    private float xVelocity;
+    private float yVelocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            keysPressed += 1;
+            yVelocity = movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            keysPressed += 1;
+            yVelocity = -movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            keysPressed += 1;
+            xVelocity = -movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            keysPressed += 1;
+            xVelocity = movementSpeed;
+        }
+
+        //GetKeyUp
+        if ((Input.GetKeyUp(KeyCode.S)) || (Input.GetKeyUp(KeyCode.W)))
+        {
+            yVelocity = 0;
+        }
+
+        if ((Input.GetKeyUp(KeyCode.A)) || (Input.GetKeyUp(KeyCode.D)))
+        {
+            xVelocity = 0;
+        }
+
+        if ((Input.GetKeyUp(KeyCode.S)) || (Input.GetKeyUp(KeyCode.W)) || (Input.GetKeyUp(KeyCode.A)) || (Input.GetKeyUp(KeyCode.D)))
+        {
+            keysPressed -= 1;
+
+            if (keysPressed == 0)
+            {
+                yVelocity = 0;
+                xVelocity = 0;
+            }
+        }
+
+        //execution
+        rb.velocity = new Vector2(xVelocity, yVelocity);
+
+        //Debuging
+        if (keysPressed > 4 || keysPressed < 0)
+        {
+            keysPressed = 0;
+        }
+    }
+}
