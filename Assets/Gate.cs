78d78eb66dd81f8cefa6d2a8cec06b50fa740839
NIFLHEIM-FROST-\Deant <dean.toledano@gmail.com>
2021-01-31 11:33:12 -0800

    complete daytime and nightime without music

---
+++ Assets/Gate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public GameManager GameManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (GameManager.gateKeyGot == (true))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
