56140a07ce1d5580bd3dea4bde22f998417ae357
LAPTOP-4IQ0NTFL\gutie <gutierrezchucho@outlook.es>
2021-01-31 01:54:47 -0800

    Main menu with credits scene

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void menu()
+    {
+        SceneManager.LoadScene("UIScene");
+    }
+    public void gameplay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void quitgame()
+    {
+        Application.Quit();
+    }
+}
